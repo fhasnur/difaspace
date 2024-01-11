5f2b30d5afc5f749bc869c83a52cfbde8a34dbab
fhasnur <fandi.hasnur10@gmail.com>
2024-01-11 23:37:56 +0800

    chore: next ui config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
+'use client';
+
+import { NextUIProvider } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Difaspace</h1>
-    </main>
+    <NextUIProvider>
+      <header>
+        <Navbar>
+          <NavbarBrand>
+            <p className="font-bold text-inherit">Difaspace</p>
+          </NavbarBrand>
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Features
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive>
+              <Link href="#" aria-current="page">
+                Customers
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Integrations
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+          <NavbarContent justify="end">
+            <NavbarItem className="hidden lg:flex">
+              <Link href="#">Login</Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Button as={Link} color="primary" href="#" variant="flat">
+                Sign Up
+              </Button>
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
+      </header>
+    </NextUIProvider>
   )
 }
